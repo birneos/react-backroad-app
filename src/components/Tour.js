825efe4d970792b524550bc9edcc20d08a1a6cb4
Marco Linke <birneos@gmx.net>
2025-04-14 12:11:02 +0200

    Titel Komponente

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,14 +1,10 @@
 import React from 'react'
+import Title from './Title'
 
 function Tour() {
   return (
     <section className="section" id="tours">
-      <div className="section-title">
-        <h2>
-          featured <span>tours</span>
-        </h2>
-      </div>
-
+      <Title title="featured" subTitle="Tours" />
       <div className="section-center featured-center">
         <article className="tour-card">
           <div className="tour-img-container">
