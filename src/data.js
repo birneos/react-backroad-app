7939545a7c028e44147f76c1f713d821e19d5585
Marco Linke <birneos@gmx.net>
2025-04-14 13:36:06 +0200

    Services und Tour zentral halten

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
 export const pageLinks = [
   {
     id: 1,
@@ -36,5 +41,69 @@ export const socialLinks = [
     id: 1,
     href: 'https://www.squarespace.com',
     icon: 'fab fa-squarespace',
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    text: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'china',
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: 'october 1th, 2020',
+    title: 'best of java',
+    text: ' Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location: 'indonesia',
+    duration: 11,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: 'october 1th, 2020',
+    title: 'best of java',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim perspiciatis saepe voluptatibus amet eligendi perferendis, architecto cupiditate. Cumque beatae voluptatum quo commodi temporibus cum quidem provident possimus, aliquam ducimus vero error consectetur animi nihil dolorem, obcaecati est dolor qui officia architecto culpa inventore tenetur? Asperiores corporis natus accusamus consectetur, assumenda suscipit inventore laboriosam quibusdam veritatis dolor. Vel libero placeat accusantium odio mollitia quam? Labore possimus quibusdam commodi vero ab officia sed eligendi sapiente perspiciatis, deserunt aperiam nemo facere quae consequuntur modi minus consequatur odio. Voluptatum suscipit, sequi quis dolorum in maiores qui explicabo eos nulla ex voluptas, cupiditate quod commodi.',
+    location: 'hong kong',
+    duration: 8,
+    cost: 5000,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: 'october 1th, 2020',
+    title: 'best of java',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim perspiciatis saepe voluptatibus amet eligendi perferendis, architecto cupiditate. Cumque beatae voluptatum quo commodi temporibus cum quidem provident possimus, aliquam ducimus vero error consectetur animi nihil dolorem, obcaecati est dolor qui officia architecto culpa inventore tenetur? Asperiores corporis natus accusamus consectetur, assumenda suscipit inventore laboriosam quibusdam veritatis dolor. Vel libero placeat accusantium odio mollitia quam? Labore possimus quibusdam commodi vero ab officia sed eligendi sapiente perspiciatis, deserunt aperiam nemo facere quae consequuntur modi minus consequatur odio. Voluptatum suscipit, sequi quis dolorum in maiores qui explicabo eos nulla ex voluptas, cupiditate quod commodi.',
+    location: 'kenya',
+    duration: 20,
+    cost: 3300,
   },
 ]
