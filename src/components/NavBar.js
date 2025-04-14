826619274d3de1dc53b3c4abe086df81ddc980f7
Marco Linke <birneos@gmx.net>
2025-04-14 14:20:53 +0200

    NavBar Komponente ergänzt mit SocialLinks und PageLinks Komponenten

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from './../images/logo.svg'
 import { pageLinks, socialLinks } from './../data.js'
+import SocialLinks from './SocialLinks.js'
+import PageLinks from './PageLinks.js'
 
 function NavBar() {
   return (
@@ -13,92 +15,10 @@ function NavBar() {
           </button>
         </div>
         {/* <!-- left this comment on purpose --> */}
-        <ul className="nav-links" id="nav-links">
-          {pageLinks.map((link) => {
-            return (
-              <li key={link.id}>
-                <a href={`#${link.text}`} className="nav-link">
-                  {link.text}
-                </a>
-              </li>
-            )
-          })}
 
-          {/* <li>
-            <a href="#home" className="nav-link">
-              {' '}
-              home{' '}
-            </a>
-          </li>
+        <PageLinks parentClass="nav-links" itemClass="nav-link" />
 
-          <li>
-            <a href="#about" className="nav-link">
-              {' '}
-              about{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link">
-              {' '}
-              services{' '}
-            </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link">
-              {' '}
-              tours
-            </a>
-          </li> */}
-        </ul>
-
-        <ul className="nav-icons">
-          {socialLinks.map((link) => {
-            return (
-              <li key={link.id}>
-                <a
-                  href={link.href}
-                  target="_blank"
-                  className="nav-icon"
-                  rel="noreferrer"
-                >
-                  <i className={link.icon}></i>
-                </a>
-              </li>
-            )
-          })}
-          {/* <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-              rel="noreferrer"
-            >
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-              rel="noreferrer"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.twitter.com"
-              target="_blank"
-              className="nav-icon"
-              rel="noreferrer"
-            >
-              <i className="fab fa-squarespace"></i>
-            </a>
-          </li> */}
-        </ul>
+        <SocialLinks parentClass="nav-icons" itemClass="nav-icon" />
       </div>
     </nav>
   )
