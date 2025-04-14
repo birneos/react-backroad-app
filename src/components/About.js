0cf9ad9ede26c52274822394176aeb7325845acf
Marco Linke <birneos@gmx.net>
2025-04-14 11:58:30 +0200

    About.js JSX needs ever ever image import

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import aboutImage from './../images/about.jpeg'
 function About() {
   return (
     <section className="section" id="about">
@@ -11,11 +11,7 @@ function About() {
 
       <div className="section-center about-center">
         <div className="about-img">
-          <img
-            src="./images/about.jpeg"
-            className="about-photo"
-            alt="awesome beach"
-          />
+          <img src={aboutImage} className="about-photo" alt="awesome beach" />
         </div>
         <article className="about-info">
           <h3>explore the difference</h3>
